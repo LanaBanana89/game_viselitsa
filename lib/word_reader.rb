012d8ee3473ce898dd4ff2e690c9933b942a4437
LanaBanana89 <32016884+LanaBanana89@users.noreply.github.com>
2018-03-02 09:48:06 +0300

    Update word_reader.rb

--- lib/word_reader.rb
+++ lib/word_reader.rb
@@ -2,16 +2,13 @@
 # из файла(метод read_from_file(file_name)) либо из консоли (read_from_args).
 # В данной игре слово считываем из файла
 class WordReader
-
   def read_from_args
     ARGV[0]
   end
 
   def read_from_file(file_name)
-
     return unless File.exist?(file_name)
     file = File.readlines(file_name, encoding: 'UTF-8')
-
     # Возвращаем рандомное слово в нижнем регистре
     Unicode::downcase(file.sample.chomp)
   end
